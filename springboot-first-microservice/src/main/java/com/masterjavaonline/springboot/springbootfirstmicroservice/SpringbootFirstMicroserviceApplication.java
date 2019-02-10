ebd4b89a41869340c7574450ae1e79475c3bcda8
balaji-singh <balu88@outlook.com>
2019-02-11 02:07:12 +0530

    Spring Boot First Micro Service

---
+++ springboot-first-microservice/src/main/java/com/masterjavaonline/springboot/springbootfirstmicroservice/SpringbootFirstMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.masterjavaonline.springboot.springbootfirstmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFirstMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFirstMicroserviceApplication.class, args);
+    }
+
+}
+
