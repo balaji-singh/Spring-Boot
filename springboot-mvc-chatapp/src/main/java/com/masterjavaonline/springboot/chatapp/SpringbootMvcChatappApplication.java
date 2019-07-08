20e56f9200ec4f1878dc8b707ae343fec7b2cf07
balaji-singh <balu88@outlook.com>
2019-07-08 17:46:08 +0530

    Spring boot mvc chat application

---
+++ springboot-mvc-chatapp/src/main/java/com/masterjavaonline/springboot/chatapp/SpringbootMvcChatappApplication.java
@@ -0,0 +1,13 @@
+package com.masterjavaonline.springboot.chatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvcChatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMvcChatappApplication.class, args);
+	}
+
+}
