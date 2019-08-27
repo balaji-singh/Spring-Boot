de55196eaa6bd3a6b1c8ec22f033b3d210f6023e
Bala S Singh <balu88@outlook.com>
2019-08-27 14:14:10 +0530

    [BAAT] Spring Boot GraphQL with Playground GUI and Annotations

---
+++ springboot-graphql-gui-annotations/src/main/java/com/masterjavaonline/graphql/SpringbootGraphqlGuiAnnotationsApplication.java
@@ -0,0 +1,13 @@
+package com.masterjavaonline.graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlGuiAnnotationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGraphqlGuiAnnotationsApplication.class, args);
+	}
+
+}
