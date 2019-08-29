bb6170435898ee3ad77cfe32a6396e8cb15ec6d9
Bala S Singh <balu88@outlook.com>
2019-08-29 21:55:00 +0530

    [BAAT] springboot-cloud-sleuth-zipkin cloud tracing

---
+++ springboot-cloud-sleuth-zipkin/src/main/java/com/masterjavaonline/zipkin/SpringbootCloudSleuthZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.masterjavaonline.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCloudSleuthZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloudSleuthZipkinApplication.class, args);
+	}
+
+}
---
+++ springboot-cloud-sleuth-zipkin/src/test/java/com/masterjavaonline/zipkin/SpringbootCloudSleuthZipkinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.masterjavaonline.zipkin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootCloudSleuthZipkinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
